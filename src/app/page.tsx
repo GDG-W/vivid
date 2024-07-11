7d428ed283088c504870809299f4045eefddcbec
Drex72 <femidavooo@gmail.com>
2024-07-11 16:49:38 +0100

    Started Carousel Implementation with SwiperJS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import { Footer } from '@/components/footer';
 import FAQs from '@/components/home/faq/faq';
-import TargetAudience from '@/components/home/target-audience/target-audience';
 import Landing from '@/components/home/landing-section/landing';
+import TargetAudience from '@/components/home/target-audience/target-audience';
 import PurchaseYourTicket from '@/components/home/ticket-details/purchase';
+import Values from '@/components/home/values/values';
 
 export default function Home() {
   return (
     <main className='main'>
       <div className='container'>
         <Landing />
+        <Values />
         <PurchaseYourTicket />
         <FAQs />
         <TargetAudience />
