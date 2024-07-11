76bed5454ab83a9771f7d7a5d2537e983e58d461
TemitopeAsama <temitopeasama@gmail.com>
2024-07-11 16:40:44 +0100

    landing page completed

--- src/components/home/landing-section/landing.tsx
+++ src/components/home/landing-section/landing.tsx
@@ -6,6 +6,10 @@ import styles from './landing.module.scss';
 import Header from '@/components/header';
 import Button from '@/components/button';
 import { classNames } from '@/utils/classNames';
+import GreenCursor from '../../../../public/green-cursor.svg';
+import YellowCursor from '../../../../public/yellow-cursor.svg';
+import RedCursor from '../../../../public/red-cursor.svg';
+import BlueCursor from '../../../../public/blue-cursor.svg';
 
 const Landing = () => {
   return (
@@ -15,10 +19,20 @@ const Landing = () => {
           <Header handleClick={() => {}} navContent={<span>Upgrade Tickets</span>} />
         </div>
 
+        <div className={styles.uppercursors}>
+          <YellowCursor />
+          <RedCursor />
+        </div>
+
         <div className={styles.herocontent}>
           <p>Don’t miss out!</p>
           <h1>Get your ticket now</h1>
           <Button text='Get Early Bird Tickets' />
+        </div>
+
+        <div className={styles.lowercursors}>
+          <GreenCursor />
+          <BlueCursor />
         </div>
 
         <div className={styles.ticketWrapper}>
