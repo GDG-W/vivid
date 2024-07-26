3010be456f8116270d2952c91f1f1549f1a74176
Drex72 <femidavooo@gmail.com>
2024-07-26 12:24:59 +0100

    started animations

--- src/components/home/landing-section/landing.tsx
+++ src/components/home/landing-section/landing.tsx
@@ -1,21 +1,47 @@
 'use client';
-import React from 'react';
-import Image from 'next/image';
-import styles from './landing.module.scss';
+import { AnimationEasings } from '@/components/animations/Easings';
+import RevealAnimation from '@/components/animations/Reveal/reveal';
 import Header from '@/components/header';
-import Button from '@/components/button';
 import { classNames } from '@/utils/classNames';
+import { CustomEase } from 'gsap/CustomEase';
+import Image from 'next/image';
+import { useRef } from 'react';
 import ArrowRight from '../../../../public/icons/arrow-right.svg';
-import GreenCursor from '../../../../public/green-cursor.svg';
-import YellowCursor from '../../../../public/yellow-cursor.svg';
-import RedCursor from '../../../../public/red-cursor.svg';
-import BlueCursor from '../../../../public/blue-cursor.svg';
+import DataAnalystCursor from '../../../../public/icons/data-analyst-cursor-icon.svg';
+import ProductDesignerCursor from '../../../../public/icons/product-designer-cursor-icon.svg';
+import ProductManagerCursor from '../../../../public/icons/product-manager-cursor-icon.svg';
+import SoftwareEngineerCursor from '../../../../public/icons/software-engineer-cursor-icon.svg';
+import styles from './landing.module.scss';
+import { useLandingPageAnimation } from './useAnimation';
 
 const Landing = () => {
+  const container = useRef<HTMLDivElement>(null);
+
+  useLandingPageAnimation(container);
+
+  const CTAText = [
+    {
+      text: 'Get',
+      delay: 0.5,
+    },
+    {
+      text: 'Early',
+      delay: 0.583,
+    },
+    {
+      text: 'Bird',
+      delay: 0.667,
+    },
+    {
+      text: 'Tickets',
+      delay: 0.75,
+    },
+  ];
+
   return (
-    <div className={styles.landing}>
+    <div ref={container} className={styles.landing}>
       <div className={styles.container}>
-        <div className={styles.headernav}>
+        <div className={classNames(styles.headernav, 'dissapear')}>
           <Header
             handleClick={() => {}}
             navContent={
@@ -27,29 +53,87 @@ const Landing = () => {
           />
         </div>
 
-        <div className={styles.uppercursors}>
-          <YellowCursor />
-          <RedCursor />
+        <div>
+          <div
+            className={classNames(
+              styles.careercursor,
+              styles['careercursor--productmanager'],
+              'dissapear',
+            )}
+          >
+            <p className={styles.careercursortext}>Product Manager</p>
+            <ProductManagerCursor />
+          </div>
+
+          <div className={classNames(styles.careercursor, styles['careercursor--productdesigner'])}>
+            <ProductDesignerCursor />
+            <p className={styles.careercursortext}>Product Designer</p>
+          </div>
         </div>
 
         <div className={styles.herocontent}>
-          <p>Don’t miss out!</p>
-          <h1>Get your ticket now</h1>
-          <Button text='Get Early Bird Tickets' />
+          <RevealAnimation
+            className={styles.landingPageCtaParagraph}
+            toAnimationOptions={{
+              delay: 0.333,
+              ease: CustomEase.create('custom', AnimationEasings.Hero.ctaHeader),
+            }}
+          >
+            <p>Don’t miss out!</p>
+          </RevealAnimation>
+
+          <RevealAnimation
+            className={styles.landingPageCtaHeader}
+            toAnimationOptions={{
+              delay: 0.5,
+              ease: CustomEase.create('custom', AnimationEasings.Hero.ctaHeader),
+            }}
+          >
+            <h1>Get your ticket now</h1>
+          </RevealAnimation>
+
+          <button className={classNames(styles.landingPageCtaButton)}>
+            {CTAText.map((item, index) => (
+              <RevealAnimation
+                key={index}
+                className={styles.landingPageCtaButtonText}
+                toAnimationOptions={{
+                  delay: item.delay,
+                  ease: CustomEase.create('custom', AnimationEasings.Hero.ctaText),
+                }}
+              >
+                <span>{item.text}</span>
+              </RevealAnimation>
+            ))}
+          </button>
         </div>
 
-        <div className={styles.lowercursors}>
-          <GreenCursor />
-          <BlueCursor />
+        <div>
+          <div
+            className={classNames(
+              styles.careercursor,
+              styles['careercursor--softwareengineer'],
+              '',
+            )}
+          >
+            <p className={styles.careercursortext}>Software Engineer</p>
+            <SoftwareEngineerCursor />
+          </div>
+
+          <div className={classNames(styles.careercursor, styles['careercursor--dataanalyst'])}>
+            <DataAnalystCursor />
+            <p className={styles.careercursortext}>Data Analyst</p>
+          </div>
         </div>
 
         <div className={styles.ticketWrapper}>
           <div className={classNames(styles.ticket, styles.one)}>
             <Image
-              src='/yellow-ticket.svg'
+              src='/green-ticket.svg'
               alt='An image of the one-day ticket'
               fill
               priority={true}
+              className={styles.ticketImage}
             />
           </div>
           <div className={classNames(styles.ticket, styles.middle)}>
@@ -58,14 +142,16 @@ const Landing = () => {
               alt='An image of the one-day ticket'
               fill
               priority={true}
+              className={styles.ticketImage}
             />
           </div>
           <div className={classNames(styles.ticket, styles.two)}>
             <Image
-              src='/green-ticket.svg'
+              src='/yellow-ticket.svg'
               alt='An image of the one-day ticket'
               fill
               priority={true}
+              className={styles.ticketImage}
             />
           </div>
         </div>
